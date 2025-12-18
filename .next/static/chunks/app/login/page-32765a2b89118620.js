baa33c2c60d9febbe89a974a70b2e0797728fc64
Florian Siebert <florian@softloop.com>
2025-12-18 16:14:57 +0700

    Deploy Tai Chi Academy website to GitHub Pages

---
+++ .next/static/chunks/app/login/page-32765a2b89118620.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[626,160,700,555,0,373,322,755,931,565,888,463,479,161,368,996,492],{4584:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=4584)}),_N_E=n.O()}]);
